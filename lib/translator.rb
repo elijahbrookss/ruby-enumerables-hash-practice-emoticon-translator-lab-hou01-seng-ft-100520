1a2f34986d85a2ac6aa42e302f649726e9b935d7
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-20 01:28:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,51 +6,11 @@ def load_library(file_location) #string
   
   new_hash = emotes.each_with_object({}) do 
     |(key, value), new_hash| 
-    
     new_hash[key.to_sym] = {
-      :english => {
-        key => value[0]
-      },
-      :japanese => {
-        key => value[1]
-      }
+      :english => value[0],
+      :japanese => value[1]
     }
 
   end
 end
-
-def get_japanese_emoticon(file_location, emote)
-  emotes = load_library(file_location)
-  emotes.each do
-    |key, value|
-    #key symbol for name
-    value[:english].each do
-      |name, emote_value|
-      if emote_value == emote then
-        return value[:japanese][name]
-      end
-    end
-  end
-  return "Sorry, that emoticon was not found"
-end
-
-def get_english_meaning(file_location, emote)
-  emotes = load_library(file_location)
-  emotes.each do
-    |key, value|
-    #key symbol for name
-    value[:japanese].each do
-      |name, emote_value|
-      if emote_value == emote then
-        return name
-      end
-    end
-  end
-  return "Sorry, that emoticon was not found"  
-end
-
-get_english_meaning("./lib/emoticons.yml", "☜(⌒▽⌒)☞")
-
-
-
 
